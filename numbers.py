8abfccb12f31b4945934d3b8740ac0163b698be2
Federico Di Claudio <federico.diclaudio@machineslikeme.com>
2025-04-30 20:24:29 -0300

    Conflicto resulto

--- numbers.py
+++ numbers.py
@@ -1,11 +1,6 @@
 # Este es el módulo numbers.py
-<<<<<<< HEAD
-def function1(fist_number, second_number):
-    return fist_number * second_number
-=======
 def function1(first_number, second_number):
     return "UCEMA " + str(first_number) + " " + str(second_number)
->>>>>>> 2cdb47443e41135f8541877351f66ddb29110041
 
 # Esta es la función 2
 def function2(first_number, second_number):
@@ -16,5 +11,4 @@ second_number = int(input("Enter the second number: "))
 result1 = function1(first_number, second_number)
 result2 = function2(first_number, second_number)
 print(f"The result of function1 is: {result1}")
-print(f"The result of function1 + 3 is: {result2+3}")
 print(f"The result of function2 is: {result2}")
