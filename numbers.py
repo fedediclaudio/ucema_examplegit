a76ed44ca2c6772d6e73c9260bfb45dd4ac84a5d
Federico Di Claudio <federico.diclaudio@machineslikeme.com>
2025-04-30 19:31:44 -0300

    Primer commit con archivo de referencia

---
+++ numbers.py
@@ -0,0 +1,12 @@
+def function1(fist_number, second_number):
+    pass
+
+def function2(first_number, second_number):
+    pass
+
+first_number = int(input("Enter the first number: "))
+second_number = int(input("Enter the second number: "))
+result1 = function1(first_number, second_number)
+result2 = function2(first_number, second_number)
+print(f"The result of function1 is: {result1}")
+print(f"The result of function2 is: {result2}")
