0fa96dda59ed637ed91e8cc1619682b3bef51272
= <=>
2025-04-30 20:27:50 -0300

    modified:   numbers.py "Actualice numbers.py poniendo una funcion"#

--- numbers.py
+++ numbers.py
@@ -6,6 +6,15 @@ def function1(first_number, second_number):
 def function2(first_number, second_number):
     return first_number + second_number
 
+def fibonacci(n):
+    if n <= 1:
+        return n
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
+# Example usage
+for i in range(10):
+    print(fibonacci(i), end=" ")
+
 first_number = int(input("Enter the first number: "))
 second_number = int(input("Enter the second number: "))
 result1 = function1(first_number, second_number)
