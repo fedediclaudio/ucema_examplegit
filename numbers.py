7e4a9e26a5965885800f64c07406ef133931d6db
nachoromero2612 <iromero26@ucema.edu.ar>
2025-04-30 20:27:14 -0300

    cambio funcion 2

--- numbers.py
+++ numbers.py
@@ -4,6 +4,7 @@ def function1(first_number, second_number):
 
 # Esta es la función 2
 def function2(first_number, second_number):
+    first_number = first_number * 2
     return first_number + second_number
 
 first_number = int(input("Enter the first number: "))
